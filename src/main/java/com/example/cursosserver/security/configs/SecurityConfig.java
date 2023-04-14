2fb5e8ec2c646f84b2c934c05d64228e3f133277
WallisonLucas13 <lucaswalison136@gmail.com>
2023-04-13 22:46:52 -0300

    release

--- src/main/java/com/example/cursosserver/security/configs/SecurityConfig.java
+++ src/main/java/com/example/cursosserver/security/configs/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.cursosserver.security.configs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,6 +27,8 @@ public class SecurityConfig {
                 .cors()
                 .and()
                 .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/api/login")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
