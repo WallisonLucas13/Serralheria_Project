6f157227f36a2d0cbde092093db513ba56cd18b6
WallisonLucas13 <lucaswalison136@gmail.com>
2023-04-14 10:01:19 -0300

    release

--- src/main/java/com/example/cursosserver/security/configs/SecurityConfig.java
+++ src/main/java/com/example/cursosserver/security/configs/SecurityConfig.java
@@ -25,6 +25,8 @@ public class SecurityConfig {
         return http
                 .csrf()
                 .disable()
+                .cors()
+                .and()
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/api/login")
                 .permitAll()
