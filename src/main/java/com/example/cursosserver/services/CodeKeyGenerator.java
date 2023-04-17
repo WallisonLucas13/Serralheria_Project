7cf95a582abcc24d9367e9a0263ff3d32ff3a4b3
WallisonLucas13 <lucaswalison136@gmail.com>
2023-04-16 23:31:48 -0300

    release

--- src/main/java/com/example/cursosserver/services/CodeKeyGenerator.java
+++ src/main/java/com/example/cursosserver/services/CodeKeyGenerator.java
@@ -11,7 +11,7 @@ public class CodeKeyGenerator {
         Random random = new Random();
 
         String keyLine = "%,&,q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,ç,x,c,v,b,n,m,.;,/,~,ç,l,o,i,p,0,9,8,7,6,5,4,3,2,1,-,=,1,!";
-        List<String> list = Arrays.asList(keyLine.split(keyLine));
+        List<String> list = Arrays.asList(keyLine.split(","));
         String keyFinish = "";
 
         for(int i=0; i<24; i++){
