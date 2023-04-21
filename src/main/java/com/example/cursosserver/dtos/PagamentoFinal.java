23319d85859ddd234d51a81a478ac569841d537c
WallisonLucas13 <lucaswalison136@gmail.com>
2023-04-21 11:33:27 -0300

    release

--- src/main/java/com/example/cursosserver/dtos/PagamentoFinal.java
+++ src/main/java/com/example/cursosserver/dtos/PagamentoFinal.java
@@ -1,10 +1,12 @@
 package com.example.cursosserver.dtos;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class PagamentoFinal {
 
     public PagamentoFinal(String valor,String formaPagamento) {
